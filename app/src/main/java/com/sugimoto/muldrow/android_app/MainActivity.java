e5c8d146fb5cf74ff022a8c7425cf88163373cdb
ben-muldrow <muldrowbr@g.cofc.edu>
2016-09-28 17:09:13 -0400

    added a modifiable textView

--- app/src/main/java/com/sugimoto/muldrow/android_app/MainActivity.java
+++ app/src/main/java/com/sugimoto/muldrow/android_app/MainActivity.java
@@ -2,12 +2,33 @@ package com.sugimoto.muldrow.android_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText editText;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // add layout objects
+        textView = (TextView)findViewById(R.id.textView);
+        editText = (EditText)findViewById(R.id.editText);
+        button = (Button)findViewById(R.id.button);
+
+        // functionality
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(editText.getText().toString());
+            }
+        });
     }
 }
