1a683c43a8dbb2e9d3d83478d3d3c57757946f5d
ben-muldrow <muldrowbr@g.cofc.edu>
2016-09-29 16:18:31 -0400

    fixed some launching issues to do with the FaceBook API not being accessed correctly

--- app/src/main/java/com/sugimoto/muldrow/android_app/MainActivity.java
+++ app/src/main/java/com/sugimoto/muldrow/android_app/MainActivity.java
@@ -7,23 +7,33 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 public class MainActivity extends AppCompatActivity {
 
+    // GUI elements
     private TextView textView;
     private EditText editText;
     private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // set creation state and view
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // start up facebook SDK and Logger [not working right now]
+        FacebookSdk.sdkInitialize(getApplicationContext());
+//      AppEventsLogger.activateApp(this);
 
         // add layout objects
         textView = (TextView)findViewById(R.id.textView);
         editText = (EditText)findViewById(R.id.editText);
         button = (Button)findViewById(R.id.button);
 
-        // functionality
+        // change text on button click
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
